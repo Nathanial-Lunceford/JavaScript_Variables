bc1db0129986d17eeb166aa7c8801783a1bbd0c7
Nate <nathanial.lunceford@gmail.com>
2022-08-09 11:11:18 -0500

    Completed Assignments

--- app.js
+++ app.js
@@ -3,8 +3,46 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
+//  README Exercises:
 // Exercise 1
 
+const firstName = "Nate";
+let lastName = String;
+var age = Number;
+
 // Exercise 2
+lastName = "Lunceford";
+age = 24;
 
 // Exercise 3
+let language = "javaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+// Assignment exercise 2:
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+// Assignment Exercise 3
+let city = "Dallas";
+let pastTime = "Playing Games";
+
+let myStore = `Hello, my name is ${fullName}. I live in ${city}. I enjoy ${pastTime}`;
+
+console.log(myStore);
